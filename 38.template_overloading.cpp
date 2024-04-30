c30e246bf2b2d67598af6920d2e2e41039e99064
sumit <sumitsah250@gmail.com>
2024-04-30 08:26:25 +0545

    class template

--- 38.template_overloading.cpp
+++ 38.template_overloading.cpp
@@ -1,9 +1,25 @@
 #include<iostream>
 using namespace std;
+template <class T>
+void display(T data){
+    cout<<data<<endl;
+}
 
+
+template< class T>
+void display(T data,int n){
+    for(int i=0;i<n;i++){
+        cout<<data<<endl;
+    }
+}
 
 
 
 int main(){
+    display(1);
+    display(1.1);
+    display(1.1,4);
+    display("Sumit",40);
 
+    return 0;
 }
