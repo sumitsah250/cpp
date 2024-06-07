ac7910536b186f5b0e46b7d98f66eafdec466904
sumit <sumitsah250@gmail.com>
2024-06-07 20:58:38 +0545

    string Stream

---
+++ 45.stringStream.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<vector>
+#include<sstream>
+#include<string>
+using namespace std;
+int main(){
+    string str;
+    cin>>str;
+    print(parseInts(str));
+    
+return 0;
+}
